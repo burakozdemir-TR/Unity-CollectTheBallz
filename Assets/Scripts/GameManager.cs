79a0579d56d8d9e228089f6d27bac13362c04554
burakozdemir-TR <ozdemir-burak98@hotmail.com>
2022-01-22 13:52:47 +0300

    Sync

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mainMenu;
+    private void Awake()
     {
-        
+        Screen.orientation = ScreenOrientation.Portrait;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void StartButton()
     {
-        
+        mainMenu.GetComponent<CanvasGroup>().DOFade(0, 1f).SetEase(Ease.InQuart);
     }
 }
